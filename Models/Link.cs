e60b2f405fe7d658a64d715132cc3fb581926fa1
MichDist <51273200+MichDist@users.noreply.github.com>
2020-11-23 18:58:49 +0100

    Added Link model

---
+++ Models/Link.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartpageApi.Models
+{
+    public class Link
+    {
+        public int id { get; set; }
+        public string HyperLink { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Clicks { get; set; }
+        public int Important { get; set; }
+        public int ImageId { get; set; }
+    }
+}
