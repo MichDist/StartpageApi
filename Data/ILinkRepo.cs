f24c8d3545e936c3aff63f9287c0351c44b80a7b
MichDist <51273200+MichDist@users.noreply.github.com>
2020-11-23 19:49:06 +0100

    Added Interface and mock class

---
+++ Data/ILinkRepo.cs
@@ -0,0 +1,14 @@
+﻿using StartpageApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartpageApi.Data
+{
+    public interface ILinkRepo
+    {
+        IEnumerable<Link> GetLinks();
+        Link GetLinkById(int id);
+    }
+}
---
+++ Data/MockLinkRepo.cs
@@ -0,0 +1,28 @@
+﻿using StartpageApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartpageApi.Data
+{
+    public class MockLinkRepo : ILinkRepo
+    {
+        Link ILinkRepo.GetLinkById(int id)
+        {
+            return new Link { id = 0, Name = "Test Link", Description = "Ein Test", HyperLink = "LINK", ImageId = 0, Clicks = 2, Important = 1 };
+        }
+
+        IEnumerable<Link> ILinkRepo.GetLinks()
+        {
+            var Links = new List<Link>
+            {
+                new Link { id = 0, Name = "Test Link0", Description = "Ein Test", HyperLink = "LINK", ImageId = 0, Clicks = 2, Important = 1 },
+                new Link { id = 1, Name = "Test Link1", Description = "Ein Test", HyperLink = "LINK", ImageId = 0, Clicks = 2, Important = 0 },
+                new Link { id = 2, Name = "Test Link2", Description = "Ein Test", HyperLink = "LINK", ImageId = 0, Clicks = 2, Important = 1 }
+        };
+
+            return Links;
+        }
+    }
+}
